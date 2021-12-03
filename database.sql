d2f5854d6f50351e3ff52452cf451882a213ae45
dommentee <chuku0929@gmail.com>
2021-12-03 15:55:50 -0500

    ALTER schema

--- database.sql
+++ database.sql
@@ -6,9 +6,24 @@ CREATE DATABASE medical_procedures;
 CREATE TABLE procedures(
     procedure_id SERIAL PRIMARY KEY,
     name VARCHAR(30),
-    price decimal
+    price decimal,
+    hospitalName VARCHAR(30),
+    hospitalCity VARCHAR(30),
+    hospitalState VARCHAR(30),
+    hospitalRating decimal,
+    healTime decimal
+
+
 );
+
 --create data INSERT
 INSERT INTO procedures (name, price) VALUES ('Facial', 120);
 INSERT INTO procedures (name, price) VALUES ('Joint Replacement', 8900);
 INSERT INTO procedures (name, price) VALUES ('Heart Bypass', 23000);
+
+--alter table
+ALTER TABLE procedures
+ADD healTime decimal;
+
+
+INSERT INTO procedures (name, price, hospitalName, hospitalCity, hospitalState, hospitalRating, healTime) VALUES ('Broken bone', 3500, 'Carney', 'Boston', 'Ma', 7.8, 46)
