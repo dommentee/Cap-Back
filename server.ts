72cbd31a66ef1711a8e3f6b7b70117a64a354ad0
dommentee <chuku0929@gmail.com>
2022-02-25 13:45:03 -0500

    test

--- server.ts
+++ server.ts
@@ -30,7 +30,7 @@ let whitelist = ['http://localhost:3000', 'https://enigmatic-retreat-81576.herok
 var corsOptions: CorsOptions = {
   credentials: true,
   origin: whitelist,
-  allowedHeaders: whitelist
+  allowedHeaders: '*'
 }
 app.use(cors(corsOptions));
 
